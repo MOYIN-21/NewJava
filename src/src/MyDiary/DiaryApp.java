e161e1559207b06e40664ad4e1a2c5a5c41e6e7b
Olawamide Moyinoluwa <olawamidemoyinoluwamary@gmail.com>
2023-09-17 03:43:15 +0100

    add

--- src/src/MyDiary/DiaryApp.java
+++ src/src/MyDiary/DiaryApp.java
@@ -7,14 +7,18 @@ import java.util.InputMismatchException;
 public class DiaryApp {
     private static String userName;
     private static String password;
-private static final Diaries myDiary = new Diaries();
+    private static String titleEdited;
+    private static String bodyEdited;
+    private static String body;
+    private static String title;
+    private static final Diaries myDiary = new Diaries();
 
     public static void main(String[] args) {
         goToMainMenu();
     }
 
-    private static void goToMainMenu(){
-        String mainMenu ="""
+    private static void goToMainMenu() {
+        String mainMenu = """
                 +++++++++++++++++++++++++++++++++++
                 Welcome to Diary
                 1 ->Create Diary
@@ -23,26 +27,27 @@ private static final Diaries myDiary = new Diaries();
                 ++++++++++++++++++++++++++++++++++++
                 """;
         String userInput = input(mainMenu);
-        switch (userInput.charAt(0)){
+        switch (userInput.charAt(0)) {
             case '1' -> createDiary();
             case '2' -> loginDiary();
             case '3' -> exit();
             default -> System.exit(1);
         }
     }
+
     private static void exit() {
         System.exit(0);
     }
 
     private static void loginDiary() {
         String user = input("Enter user name");
-        if (!user.equals(userName)){
+        if (!user.equals(userName)) {
             JOptionPane.showMessageDialog(null, "User name not found");
             loginDiary();
         }
         String security = input("Enter your password");
-        if (!security.equals(password)){
-            JOptionPane.showMessageDialog(null,"Your password did not match your user name, try again");
+        if (!security.equals(password)) {
+            JOptionPane.showMessageDialog(null, "Your password did not match your user name, try again");
             loginDiary();
         }
         mainDiaryMenu();
@@ -50,59 +55,98 @@ private static final Diaries myDiary = new Diaries();
 
     private static void createDiary() {
         String userName = input("Enter your user name");
-        if (userName.isEmpty()){
+        if (userName.isEmpty()) {
             JOptionPane.showMessageDialog(null, "user name can not be empty");
             createDiary();
         }
         String password = input("Enter password for unlocking your diary");
-        if (password.isEmpty()){
+        if (password.isEmpty()) {
             JOptionPane.showMessageDialog(null, "Password can not be empty");
             createDiary();
         }
         myDiary.addUser(userName, password);
         String message = "Hello" + " " + userName + " your Diary has been created successfully.";
         JOptionPane.showMessageDialog(null, message);
-        loginDiary();
+        mainDiaryMenu();
     }
 
 
     private static void mainDiaryMenu() {
-        String mainMenu ="""
+        String mainMenu = """
                 +++++++++++++++++++++++++++++++++++
                 1 ->Create Entry
                 2 ->Find Entry
                 3 ->Delete Entry
                 4 ->Update Entry
                 5 ->Lock Diary
-                7 ->Exit
+                6 ->Exit
                 ++++++++++++++++++++++++++++++++++++
                 """;
         String userInput = input(mainMenu);
-        switch (userInput.charAt(0)){
+        switch (userInput.charAt(0)) {
             case '1' -> createEntry();
             case '2' -> findEntry();
             case '3' -> deleteEntry();
             case '4' -> updateEntry();
-//            case '5' -> LockDiary();
-//            case '6' -> unlockDiary();
-//            case '7' -> Exit();
-            default -> exit();
+            case '5' -> lockDiary();
+            case '6' -> exit();
+            default -> logout();
+        }
+    }
+
+    private static void logout() {
+        System.exit(0);
+    }
+
+    private static void lockDiary() {
+        String userPassword = input("Enter password to lock");
+        if (!userPassword.equals(password)){
+            throw new InputMismatchException("Incorrect password");
+        }
+        JOptionPane.showMessageDialog(null, "Diary is locked");
+        String response = input("Enter Yes to back to diary / No ");
+        if (response.equalsIgnoreCase("Yes")) {
+            mainDiaryMenu();
+        }
+        else {
+            System.exit(0);
         }
     }
 
     private static void updateEntry() {
         String id = input("Enter id");
         if (!id.matches("[0-9]+")) {
-            JOptionPane.showMessageDialog(null, "Id should be number, try again");
-            createDiary();
+            JOptionPane.showMessageDialog(null, "Id should be number, try again");}
+        if (id.equals(0)){
+            updateEntry();
+            throw new NullPointerException("Entry not found");
         }
-        String title = input("Enter titled");
-        String body = input("Enter body");
+        myDiary.findByUserName(userName).findEntry(Integer.parseInt(id)).getEntry();
         myDiary.findByUserName(userName).updateEntry(Integer.parseInt(id), title, body);
-        String message = "Entry updated successfully";
-        JOptionPane.showMessageDialog(null, message);
 
+        String editTitle = String.valueOf(JOptionPane.showInputDialog("Enter yes to edit title"));
+        if (editTitle.equalsIgnoreCase("Yes")){
+            JOptionPane.showMessageDialog(null, title);
+            titleEdited = JOptionPane.showInputDialog("Edit");
+        }
+        if (editTitle.equalsIgnoreCase("No")){
+            mainDiaryMenu();
+        }
+        String editBody = input("Enter yes to edit body");
+        if (editBody.equalsIgnoreCase("Yes")){
+            JOptionPane.showMessageDialog(null, body);
+            bodyEdited = JOptionPane.showInputDialog("Edit");
+        }
+        if (editBody.equalsIgnoreCase("No")){
+            mainDiaryMenu();
+        }
+        JOptionPane.showMessageDialog(null, "Entry successfully updated");
 
+        int confirmUpdate = Integer.parseInt(input("Enter entry id to see entry updated"));
+        myDiary.findByUserName(userName).findEntry(confirmUpdate).getEntry();
+        JOptionPane.showMessageDialog(null, titleEdited);
+        JOptionPane.showMessageDialog(null, bodyEdited);
+        mainDiaryMenu();
     }
 
     private static void deleteEntry() {
@@ -121,6 +165,7 @@ private static final Diaries myDiary = new Diaries();
         String id = input("Enter id");
         if (!id.matches("[0-9]+")) {
             JOptionPane.showMessageDialog(null, "Id should be number, try again");}
+        findEntry();
         if (id.equals(0)){
             findEntry();
             throw new InputMismatchException("Invalid id");
@@ -135,7 +180,7 @@ private static final Diaries myDiary = new Diaries();
         String id = input("Enter id");
         if (!id.matches("[0-9]+")) {
             JOptionPane.showMessageDialog(null, "Id should be number, try again");
-            createDiary();
+            createEntry();
         }
         String title = input("Enter titled");
         String body = input("Enter body");
@@ -150,5 +195,4 @@ private static final Diaries myDiary = new Diaries();
     private static String input(String Prompt){
         return JOptionPane.showInputDialog(Prompt);
     }
-
 }
